1eb2920af79f07c95cdadd89b1b8559cba4b3185
ginlov <giangvl.cs@gmail.com>
2026-04-10 06:00:01 +0000

    Deploying to gh-pages from @ ginlov/ginlov.github.io@737928f94d2c390198a42c920d3c086f5a4b127d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -73,6 +73,16 @@ ninja.data = [{
           description: "a Deep Learning Infrastructure Project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-ece276a-sensing-amp-estimation-in-robotics",
+          title: 'ECE276A — Sensing &amp;amp; Estimation in Robotics',
+          description: "a Robotics project",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-mae204-mobile-manipulation-with-the-youbot",
+          title: 'MAE204 — Mobile Manipulation with the youBot',
+          description: "Feedforward + PI task-space control for pick-and-place on a mobile manipulator",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
             },},{
         id: 'social-email',
         title: 'email',
